98941b0b94b3cd6573e41ab37d6a2b8530daceed
Isaac Ramirez <isaacramirezgarrido1@gmail.com>
2022-06-05 16:35:53 +0200

    init project

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+
+const list = [
+  {id: 0, text: "do something", completed: false},
+  {id: 1, text: "speak with someone", completed: true},
+  {id: 2, text: "play the guitar", completed: false},
+  {id: 3, text: "write a book", completed: false},
+]
+
+function App() {
+  return (
+    <React.Fragment>
+      
+    </React.Fragment>
+  );
+}
+
+export default App;
